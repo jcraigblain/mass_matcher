135f2ece32020d8d654d68c1bce406af369dbc6f
Craig Blain <craig.blain@gmail.com>
2012-11-21 22:30:32 -0500

    changed root directory to form

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ MassMatcher::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'mass_matcher_inputs#form'
 
   # See how all your routes lay out with "rake routes"
 
